23b123658d8236542163368c02f433f0101745fa
Luis Delgado <44483231+Luis-D@users.noreply.github.com>
2020-05-22 17:19:44 -0400

    Cambios menores y chequeos de datos

--- Script.ps1
+++ Script.ps1
@@ -8,7 +8,6 @@ $main_form.AutoSize = $false
 $main_form.FormBorderStyle = 'FIxedDIalog'
 
 #GUI para procesos
-
 $texbox = New-Object System.Windows.Forms.DataGridView
 $texbox.Width = 650
 $texbox.Height = 270
@@ -21,9 +20,6 @@ $texbox.ColumnHeadersVisible = $true
 $texbox.RowHeadersVisible = $false
 $texbox.AutoSizeColumnsMode = 'Fill'
 $texbox.ScrollBars = "Vertical"
-
-
-
 $main_form.Controls.Add($texbox)
 #--------------------------------------------------------
 
@@ -36,13 +32,8 @@ $Button = New-Object System.Windows.Forms.Button
 $Button.Location = New-Object System.Drawing.Size(10,10)
 $Button.Size = New-Object System.Drawing.Size(120,23)
 $Button.Text = "Escanear Procesos"
-
 $main_form.Controls.Add($Button)
-
-$Button.Add_Click({
-    $global:MODE=0
-
-})
+$Button.Add_Click({$global:MODE=0})
 
 $Buttoncpu = New-Object System.Windows.Forms.Button
 $Buttoncpu.Location = New-Object System.Drawing.Size(140,10)
@@ -51,68 +42,64 @@ $Buttoncpu.Text = "CPU > 10%"
 
 $main_form.Controls.Add($Buttoncpu)
 
-$Buttoncpu.Add_Click({
-    $global:MODE=1
-
-})
+$Buttoncpu.Add_Click({$global:MODE=1})
 $Buttonmem = New-Object System.Windows.Forms.Button
 $Buttonmem.Location = New-Object System.Drawing.Size(270,10)
 $Buttonmem.Size = New-Object System.Drawing.Size(120,23)
 $Buttonmem.Text = "RAM > 8%"
-
 $main_form.Controls.Add($Buttonmem)
-
-$Buttonmem.Add_Click({
-   $global:MODE=2
-})
+$Buttonmem.Add_Click({$global:MODE=2})
 
 $Buttonbu = New-Object System.Windows.Forms.Button
 $Buttonbu.Location = New-Object System.Drawing.Size(400,10)
 $Buttonbu.Size = New-Object System.Drawing.Size(120,23)
 $Buttonbu.Text = "Terminar Proceso"
-
 $main_form.Controls.Add($Buttonbu)
     
-    
 $Buttonbu.Add_Click({
-    Stop-Process ($texbox.Rows[$texbox.CurrentRow.Index].Cells[1].Value);
-    
+    $i = $texbox.CurrentRow.Index;  
+    if($i -ne $null)
+    {Stop-Process ($texbox.Rows[$i].Cells[1].Value);}
 })
 
 $Buttonb = New-Object System.Windows.Forms.Button
 $Buttonb.Location = New-Object System.Drawing.Size(530,10)
 $Buttonb.Size = New-Object System.Drawing.Size(120,23)
 $Buttonb.Text = "Terminar Procesos"
-
 $main_form.Controls.Add($Buttonb)
 
-$Buttonb.Add_Click({
-    $global:MODE=3
-
-})
+$Buttonb.Add_Click({$global:MODE=3})
 
 #--------------------------------------------------------
-#(Get-WmiObject -class Win32_PerfFormattedData_PerfProc_Process)
+$nombres_de_windows=  "powershell", "cmd","ApplicationFrameHost", "MicrosoftEdge",
+"WindowsInternal", "WinStore.App","SystemSettings", 
+"WindowsInternal.ComposableShell.Experiences.TextInput.InputApp", 
+"MicrosoftEdgeCP", "MicrosoftEdge"
+$global:ArrayList = New-Object System.Collections.ArrayList
 
 $global:pwshv = ((Get-Host).Version.Major)
 $global:rambyte =((Get-WmiObject Win32_ComputerSystem).totalphysicalmemory)
 
 function GET_STAMP
 {
-    $gp = (Get-WmiObject -class Win32_PerfFormattedData_PerfProc_Process | Where-Object{ $_.name -inotmatch '_total|idle'})
-    
-    $gp= $gp | foreach-object{  
-        $tmpPID = $_.IDProcess;
+    $gp = gps | ? {$_.mainwindowtitle.length -ne 0} | where-object {$nombres_de_windows -notcontains $_.ProcessName}
+    foreach($x in $gp){$global:ArrayList.add($x.Id)}
+
+    $gp = Get-WmiObject Win32_PerfFormattedData_PerfProc_Process | where-object{
+    $global:ArrayList -eq $_.IDProcess
+    }
+    $global:ArrayList.Clear()
+
+    $w= $gp | foreach-object{  
         $tmp=@{
-            PID=$tmpPID;
-            Nombre=(($_.Name).split("#"))[0];
+            PID=$_.IDProcess;
+            Nombre=$_.Name;
             RAM= [math]::round((($_.WorkingSetPrivate/$global:rambyte)*100.0),3);
             CPU= $_.PercentProcessorTime;
         }
         New-Object -TypeName PSObject -prop $tmp;
-    }
-   
-    return $gp
+    }    
+    return $w
 }
 
 Function Stop
@@ -124,17 +111,13 @@ Function Stop
     return $PROCESOS
 }
 
-
 Function Info
 {
     param($CPUMin,$RAMMin)
-    filter OK {
-        if( $_.RAM -GE $RAMMin -and 
-            $_.CPU -GE $CPUMin -and 
-            (Get-Process -PID $_.PID).Sessionid -ne 0)
-        {$_}
-     }
-    return (GET_STAMP | OK); 
+    if($CPUMin -eq 0.0 -and $RAMMin -eq 0.0)
+    {filter OK {if( $_.RAM -ge $RAMMin -and $_.CPU -ge $CPUMin){$_}}}
+    else{filter OK {if( $_.RAM -gt $RAMMin -and $_.CPU -gt $CPUMin){$_}}}
+    return (GET_STAMP | OK);
 }
 
 Function GET_DATA
@@ -147,37 +130,29 @@ Function GET_DATA
     if($mode -eq 3){$Object = (Stop(Info 10.0 8.0)); return $Object} #eliminar los procesos con uso de 10%cpu y 8%ram del computador
 }
 
-
 $global:currentIndex=0;
 
 Start-Sleep -m 500
 
 $timer = new-OBject System.Windows.Forms.Timer
-$timer.Interval = 600
+$timer.Interval = 2000
 $timer.add_tick({Update})  
 $timer.start()
 
 $DATA = GET_DATA($global:MODE)
 
-
 Function Update()
 {
- $DATA = GET_DATA($global:MODE)
- 
+   $DATA = GET_DATA($global:MODE)
 try{
     $global:currentIndex = $texbox.CurrentRow.Index;
     $currentCol = $texbox.CurrentCol.Index;
-    $currentRow = $texbox.FirstDisplayedScrollingRowIndex;
-    
+    $currentRow = $texbox.FirstDisplayedScrollingRowIndex;    
     $texbox.datasource = [collections.arraylist]$DATA;
-    
     $texbox.CurrentCell = $texbox.Rows[$global:currentIndex].Cells[0];
-    $texbox.FirstDisplayedScrollingRowIndex = $currentRow;
-    
+    $texbox.FirstDisplayedScrollingRowIndex = $currentRow;  
     $texbox.update();
     }
     catch{}  
-    
 }
-
 $main_form.ShowDialog();
